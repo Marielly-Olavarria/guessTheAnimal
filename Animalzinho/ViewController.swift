b016eee3602ae6df61d298958de7e14e14c92ddd
marielly-olavarria <olavarriadanielmari@gmail.com>
2022-01-25 17:53:03 -0800

    melhorias de layout do app

--- Animalzinho/ViewController.swift
+++ Animalzinho/ViewController.swift
@@ -8,37 +8,81 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    var animals: [String] = ["golfinho", "leão", "gato", "capivara", "ornitorrinco"]
+    var count: Int = 0
+    var points: Int = 0
+    var alert = UIAlertController()
+    
+    // MARK: - Screen properties
+    @IBOutlet weak var imageField: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var textField: UITextField!
+    
+    // MARK: - didLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        campoImagem.image = UIImage(named: "golfinho_fofinho")
-        errorMessage.addAction(UIAlertAction(title: "Ok",style: .default, handler: { (action: UIAlertAction!) in }))
-        rightAnswerMessage.addAction(UIAlertAction(title: "Ok",style: .default, handler: { (action: UIAlertAction!) in }))
-    }
-
-    let descubraOAnimalzinho: String = "golfinho_fofinho"
-    let errorMessage = UIAlertController(title: "Atenção", message: "Presta atenção seu burro", preferredStyle: .alert)
-    let rightAnswerMessage = UIAlertController(title: "Atenção", message: "10 pontos para a Grifinória", preferredStyle: .alert)
+        animals.shuffle()
+        imageField.image = UIImage(named: animals[count])
+            }
     
-    @IBOutlet weak var campoImagem: UIImageView!
-    @IBOutlet weak var button: UIButton!
-     
-    @IBOutlet weak var golfinho_fofinho: UITextField!
+    // MARK: - Functions
+    func increaseCountAnUpdate() {
+        if count < animals.count - 1 {
+            count = count + 1
+            imageField.image = UIImage(named: animals[count])
+        } else {
+            gameOver()
+        }
+    }
+    
+    func gameOver() {
+        alert = UIAlertController(title: "FIM DE JOGO!", message: "Sua pontuação final foi de \(points) pontos", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
+            self.restartGame()
+        }))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func restartGame(){
+        self.count = 0
+        self.points = 0
+        self.animals.shuffle()
+        self.imageField.image = UIImage(named: self.animals[self.count])
+    }
+    
+    // MARK: - Actions
     @IBAction func carregarImagem(_ sender: Any) {
-       
-        if descubraOAnimalzinho == golfinho_fofinho.text {
-            self.present(rightAnswerMessage, animated: true, completion: nil)
+        
+        if  textField.text?.uppercased() == animals[count].uppercased() {
+            points = points + 10
+            alert = UIAlertController(title: "Atenção", message: "10 pontos para a Grifinória! Sua pontuação atual é \(points)", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok",style: .default, handler: { (action: UIAlertAction!) in
+                self.increaseCountAnUpdate()
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
         }
         else {
-            self.present(errorMessage, animated: true, completion: nil)
+            if points > 0 {
+                points = points - 5
+            }
+            alert = UIAlertController(title: "Atenção", message: "Presta atenção seu burro!Sua pontuação atual é \(points)",  preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok",style: .default, handler: { (action: UIAlertAction!) in
+                self.increaseCountAnUpdate()
+            }))
+            self.present(alert, animated: true, completion: nil)
         }
+        textField.text = ""
+        
     }
     
     
-
-
-
-
+    
+    
+    
+    
 }
 
 
